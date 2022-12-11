d5be2092e25440475e449409719f289456c61c9f
GuiMello83 <gui.vazdemello@gmail.com>
2022-12-10 21:29:15 -0300

    commit

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react"
+
 import styles from "../../src/Components/Form.module.css";
 
-const LoginForm = () => {
+const LoginForm = () => { 
+
+  const [userName, setUserName] = useState('')
+  const [userPassword, setUserPassword] = useState('')
+
+
+
+
   const handleSubmit = (e) => {
     //Nesse handlesubmit você deverá usar o preventDefault,
     //enviar os dados do formulário e enviá-los no corpo da requisição 
@@ -9,6 +18,9 @@ const LoginForm = () => {
     //no localstorage para ser usado em chamadas futuras
     //Com tudo ocorrendo corretamente, o usuário deve ser redirecionado a página principal,com react-router
     //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erro
+
+    event.preventDefault()
+    
   };
 
   return (
